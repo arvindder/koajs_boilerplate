bb4006aa8d970fa9a2f7b02b292190f7fd63f86a
Arvindder Rajasingam <arvin6730@gmail.com>
2019-05-06 10:55:18 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const koa = require('koa');
+const json = require('koa-json');
+const koa_router = require('koa-router');
+const path = require('path');
+const render = require('koa-ejs');
+
+const app = new koa();
+const router = new koa_router();
+
+//Json prettier Middleware
+app.use(json());
+
+
+render(app,{
+    root: path.join(__dirname,'views'),
+    layout: 'layout',
+    viewExt: 'html',
+    cache: false,
+    debug: false
+});
+
+//Simple Middlware
+// app.use(async ctx => ctx.body = {msg:'Hello World'});
+
+router.get('/', async ctx => {
+    await ctx.render('index');
+})
+
+//Router
+router.get('/test', (ctx,next) => {
+    ctx.body = "Hello World";
+});
+
+//Router middleware
+app.use(router.routes()).use(router.allowedMethods());
+
+//Initializing App
+app.listen(3000,() => console.log('Server Started....'));
